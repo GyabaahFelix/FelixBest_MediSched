724c819d2bedec115f278c4e9112bba9b7ecd880
GyabaahFelix <felixic360@gmail.com>
2024-12-25 18:20:17 +0000

    2nd commit on christmas day

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,9 +2,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import AdminContextProvider from "./context/AdminContext.jsx"; // Correct import
+import DoctorContextProvider from "./context/DoctorContext.jsx";
+import AppContextProvider from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <AdminContextProvider> {/* Use AdminContextProvider here */}
+        <DoctorContextProvider>
+          <AppContextProvider>
+            <App />
+          </AppContextProvider>
+        </DoctorContextProvider>
+      </AdminContextProvider>
+    </BrowserRouter>
 );
