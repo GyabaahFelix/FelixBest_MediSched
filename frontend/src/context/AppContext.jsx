e6d97f4e6efd8f2fe0d13875f8adee447a4b67e2
GyabaahFelix <felixic360@gmail.com>
2024-12-28 21:08:29 +0000

    dec28

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -8,12 +8,10 @@ const AppContextProvider = (props) => {
 
     const currencySymbol = '$'
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
     const [doctors,setDoctors] = useState([])
+    const [token,setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):false) 
     
-    const value = {
-        doctors,
-        currencySymbol
-    }
 
     const getDoctorData = async () => {
         
@@ -32,6 +30,13 @@ const AppContextProvider = (props) => {
         }
 
     }
+
+    const value = {
+        doctors,
+        currencySymbol,
+        token,setToken,
+        backendUrl
+    } 
 
     useEffect(()=>{
         getDoctorData()
