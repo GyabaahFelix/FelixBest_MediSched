ab493805405a9e09647b5a72a29d36a33a389391
GyabaahFelix <felixic360@gmail.com>
2025-01-03 21:23:59 +0000

    dec3

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -9,6 +9,7 @@ const AdminContextProvider = (props) => {
         localStorage.getItem('aToken') ? localStorage.getItem('aToken') : ''
     );
     const [doctors,setDoctors] = useState([])
+    const [appointments, setAppointments] = useState([])
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
@@ -48,11 +49,52 @@ const AdminContextProvider = (props) => {
         
     }
 
+    const getAllAppointments = async () => {
+
+        try {
+
+            const {data} = await axios.get(backendUrl+'/api/admin/appointments', {headers:{aToken}})
+
+            if (data.success) {
+                setAppointments(data.appointments)
+                console.log(data.appointments)
+            } else {
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            toast.error(error.message)
+        }
+        
+    }
+
+    const cancelAppointment = async (appointmentId) => {
+        
+        try {
+
+            const {data} = await axios.post(backendUrl+'/api/admin/cancel-appointment',{appointmentId},{headers:{aToken}})
+
+            if (data.success) {
+                toast.success(data.message)
+                getAllAppointments()
+            } else {
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            toast.error(error.message)
+        }
+
+    }
+
     const value = {
         aToken, // Updated to match the naming in Navbar
         setAtoken,
         backendUrl,doctors,
         getAllDoctors, changeAvailability,
+        appointments,setAppointments,
+        getAllAppointments,
+        cancelAppointment
     };
 
     return (
