838d68707eb0ce3c256452e4373f0fc8b4a09313
GyabaahFelix <felixic360@gmail.com>
2025-01-07 17:06:19 +0000

    2nd commit on constitution day

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,6 @@ import jwt from "jsonwebtoken";
 import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
 import appointmentModel from "../models/appointmentModel.js";
-import razorpay from "razorpay";
 
 // API to register user
 const registerUser = async (req, res) => {
@@ -223,41 +222,6 @@ const cancelAppointment = async (req, res) => {
   }
 };
 
-const razorpayInstance = new razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
-
-//  API to make payment of appointment using razorpay
-const paymentRazorPay = async (req, res) => {
-  try {
-    const { appointmentId } = req.body;
-    const appointmentData = await appointmentModel.findById(appointmentId);
-
-    if (!appointmentData || appointmentData.cancelled) {
-      return res.json({
-        success: false,
-        message: "Appointment Cancelled or not found",
-      });
-    }
-
-    // creating options for razorpay payment
-    const options = {
-      amount: appointmentData.amount * 100,
-      currency: process.env.CURRENCY,
-      receipt: appointmentId,
-    };
-
-    // creation of an order
-    const order = await razorpayInstance.orders.create(options);
-
-    res.json({ success: true, order });
-  } catch (error) {
-    console.log(error);
-    res.json({ success: false, message: error.message });
-  }
-};
-
 export {
   registerUser,
   loginUser,
@@ -266,5 +230,4 @@ export {
   bookAppointment,
   listAppointment,
   cancelAppointment,
-  paymentRazorPay
 };
--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -7,18 +7,17 @@ const MyAppointments = () => {
 
   const { backendUrl, token, getDoctorData } = useContext(AppContext)
 
-  const [appointments,setAppointments] = useState([])
-  const months = [" ","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+  const [appointments, setAppointments] = useState([])
+  const months = [" ", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
   const slotDateFormat = (slotDate) => {
     const dateArray = slotDate.split('_')
-    return dateArray[0]+ " " + months[Number(dateArray[1])] + " " + dateArray[2]
+    return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
   }
 
   const getUserAppointments = async () => {  
     try {
-
-      const {data} = await axios.get(backendUrl+'/api/user/appointments',{headers:{token}})
+      const { data } = await axios.get(backendUrl + '/api/user/appointments', { headers: { token } })
 
       if (data.success) {
         setAppointments(data.appointments.reverse())
@@ -29,14 +28,11 @@ const MyAppointments = () => {
       console.log(error)
       toast.error(error.message)
     }
-
   }
 
   const cancelAppointment = async (appointmentId) => {
-
     try {
-
-      const {data} = await axios.post(backendUrl + '/api/user/cancel-appointment', {appointmentId},{headers:{token}})
+      const { data } = await axios.post(backendUrl + '/api/user/cancel-appointment', { appointmentId }, { headers: { token } })
       if (data.success) {
         toast.success(data.message)
         getUserAppointments()
@@ -49,36 +45,19 @@ const MyAppointments = () => {
       console.log(error)
       toast.error(error.message)
     }
-    
   }
 
-  const appointmentRazorpay = async (appointmentId) => {
-
-    try {
-
-      const {data} = await axios.post(backendUrl+'/api/user/payment-razorpay',{appointmentId},{headers:{token}})
-
-      if (data.success) {
-        console.log(data.order)
-      }
-      
-    } catch (error) {
-      
-    }
-    
-  }
-
-  useEffect(()=>{
+  useEffect(() => {
     if (token) {
       getUserAppointments()
     }
-  },[token])
+  }, [token])
 
   return (
     <div>
       <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My Appointments</p>
       <div>
-        {appointments.map((item,index)=>(
+        {appointments.map((item, index) => (
           <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
             <div>
               <img className='w-32 bg-indigo-50' src={item.docData.image} alt="" />
@@ -94,11 +73,9 @@ const MyAppointments = () => {
             <div></div>
             <div className='flex flex-col gap-2 justify-end'>
               {!item.cancelled && item.payment && !item.isCompleted && <button className='sm:min-w-48 py-2 border rounded text-stone-500 bg-indigo-50'>Paid</button>}
-              {!item.cancelled && !item.isCompleted &&  <button onClick={()=>appointmentRazorpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
-              {!item.cancelled && !item.isCompleted &&  <button onClick={()=>cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>}
+              {!item.cancelled && !item.isCompleted &&  <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>}
               {item.cancelled && !item.isCompleted &&  <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment Cancelled</button>}
-              {item.isCompleted && <button className='sm:min-w-48 py-2
-               border border-green-500 rounded text-green-500'>Completed</button>}
+              {item.isCompleted && <button className='sm:min-w-48 py-2 border border-green-500 rounded text-green-500'>Completed</button>}
             </div>
           </div>
         ))}
